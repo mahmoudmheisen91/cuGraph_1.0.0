a15d365a1ba93a8d1496cc211ea725346050f73b
Mahmoud Mheisen <mahmoudmheisen91@gmail.com>
2015-05-29 09:22:28 +0200

    modify device kernal to be seeded by time(0)

--- cuda/main.hpp
+++ cuda/main.hpp
@@ -7,14 +7,17 @@
 #include <iomanip>
 #include <string>
 #include <cuda.h>
+#include <ctime>
+#include <algorithm>
 
-#define N 8
+#define N 1024
 #define gridSize 1
 #define blockSize N
 
 using namespace std;
 
-__global__ void random_number_generator_kernal(int *masterSeed, int *itemsPerThread, int *PRNG);
+__global__ void random_number_generator_kernal(int *masterSeed, int *itemsPerThread, float *PRNG);
+
 
 #endif
 
