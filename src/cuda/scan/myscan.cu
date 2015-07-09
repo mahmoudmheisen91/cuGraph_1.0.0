bd52f1791bed940d9688c5fe9ff9703a1ab0b16c
Mahmoud Mheisen <mahmoudmheisen91@gmail.com>
2015-07-09 06:45:15 +0200

    add kernel 3 to add block_result from kernel 2 to data in kernel 1

--- src/cuda/scan/myscan.cu
+++ src/cuda/scan/myscan.cu
@@ -16,45 +16,56 @@ int main() {
 	int size = 1024*1024;
 	
 	// allocate host:
-	int *data_in_host = NULL;
-	data_in_host = new int[size];
-	
-	int *data_out_host = NULL;
-	data_out_host = new int[size];
+	int *data_host = NULL;
+	data_host = new int[size];
 	
 	// allocate device:
-	int *data_in_device = NULL;
-	cudaMalloc((void**) &data_in_device, size * sizeof(int));
+	int *data_device = NULL;
+	cudaMalloc((void**) &data_device, size * sizeof(int));
 	
 	int *block_results = NULL;
 	cudaMalloc((void**) &block_results, 1024 * sizeof(int));
 	
 	// fill host:
 	for(int i = 0; i < size; i++) {
-		data_in_host[i] = 1;
+		data_host[i] = 1;
 	}
 	
 	// copy host to device:
-	cudaMemcpy(data_in_device, data_in_host, size * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(data_device, data_host, size * sizeof(int), cudaMemcpyHostToDevice);
 	
 	// kernel:
-	global_scan_kernel_1 <<<1024, 1024>>> (data_in_device, block_results);
+	
+	float time;
+    cudaEvent_t start, stop; 
+
+    cudaEventCreate(&start); 
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0); 
+
+	global_scan_kernel_1 <<<1024, 1024>>> (data_device, block_results);
 	global_scan_kernel_2 <<<1, 1024>>> (block_results);
-	global_scan_kernel_3 <<<1024, 1024>>> (data_in_device, block_results);
+	global_scan_kernel_3 <<<1024, 1024>>> (data_device, block_results);
+	
+    cudaEventRecord(stop, 0); 
+    cudaEventSynchronize(stop);  
+    cudaEventElapsedTime(&time, start, stop); 
+    cudaEventDestroy(start); 
+    cudaEventDestroy(stop);
 	
 	// copy device to host:
-	cudaMemcpy(data_out_host, data_in_device, size * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(data_host, data_device, size * sizeof(int), cudaMemcpyDeviceToHost);
 	
 	// print:
+	cout << "time = " << time << endl;
 	for(int i = size-1; i < size; i++) {
-		cout << data_out_host[i] << " ";
+		cout << data_host[i] << " ";
 	}
 	cout << endl;
 	
 	// free:
-	delete data_in_host;
-	delete data_out_host;
-	cudaFree(data_in_device);
+	delete data_host;
+	cudaFree(data_device);
 	
 	// end:
 	return 0;
@@ -99,7 +110,6 @@ __device__ int single_block_scan(int *data_in, int idx) {
 	}
 	__syncthreads ();
 
-	
 	// Step 4: Accumulate results from Steps 1 and 3
 	if (warpid > 0) val = data_in[(warpid - 1)+blockIdx.x*1024] + val;
 	__syncthreads ();
